57ca867f9e30ac7e8f5d7b8de892f01ff4d5cc85
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-02-25 11:59:52 +0100

    Add files via upload

--- MiPrimerProyectoJava/src/HelloWorld.java
+++ MiPrimerProyectoJava/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		byte cont;
 		byte numero=5;
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hola, mundo");
 		for (cont=0; cont<=10; cont++){
 			System.out.print(cont);
