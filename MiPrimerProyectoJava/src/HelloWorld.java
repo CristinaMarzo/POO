f192d9856e88d6648bae70b4cef846207ec215c9
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-02-25 11:59:38 +0100

    Add files via upload

--- MiPrimerProyectoJava/src/HelloWorld.java
+++ MiPrimerProyectoJava/src/HelloWorld.java
@@ -3,7 +3,17 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		byte cont;
+		byte numero=5;
+		System.out.println("Hola, mundo");
+		for (cont=0; cont<=10; cont++){
+			System.out.print(cont);
+			System.out.print(" X ");
+			System.out.print(numero);
+			System.out.print(" = ");
+			System.out.println(cont*numero);
+		
+		}
 	}
 
 }
