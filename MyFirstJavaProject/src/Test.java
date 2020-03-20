ac2c95ae1da131f598b30673f62ebe4bc7b94d95
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-03-20 14:35:57 +0100

    Add files via upload

---
+++ MyFirstJavaProject/src/Test.java
@@ -0,0 +1,12 @@
+
+class Animal {}
+class Dog extends Animal {}
+class Cat extends Animal {}
+
+public class Test
+{
+    // usando el objeto Dog
+    Animal obj1 = new Dog(),
+    // usando el objeto Cat
+    obj2 = new Cat();
+}
