09b22647356752e9b3e83c5ddffe786c49b27582
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-03-25 17:02:58 +0100

    Delete Vehiculo.java

--- MyFirstJavaProject/src/Vehiculo.java
+++
@@ -1,6 +0,0 @@
-
-public class Vehiculo {
-	    int pasajeros;  //números de pasajeros
-	    int capacidad;  //capacidad del combustible en galones
-	    int mpg;        //combustible consumido en millas por galon
-}
