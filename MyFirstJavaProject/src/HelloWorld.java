be18250c4617e1b4c8b4755a49a58a499405efcd
Cristina Marzo <55531207+CristinaMarzo@users.noreply.github.com>
2020-03-30 21:07:13 +0200

    Scanner knowledge.

--- MyFirstJavaProject/src/HelloWorld.java
+++ MyFirstJavaProject/src/HelloWorld.java
@@ -1,15 +1,16 @@
-//import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner;  // Import the Scanner class
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
 		byte cont;
-		byte numero=5;
+		int numero;
 		System.out.println("Bienvenido al programa para calcular las tablas de multiplicar.");
 		System.out.println("Introduzca el numero deseado: ");
-		//Scanner scanner = new Scanner(System.in); //esto seria un cin de c++
-
+		try (Scanner scanner = new Scanner(System.in)) {
+			numero = scanner.nextInt();
+		}
 		System.out.println("Hola, mundo");
 		for (cont=0; cont<=10; cont++){
 			System.out.print(cont);
